59884391ef9f3eee1271cadd3730ad8ea4c73fd3
Arthur Axel "fREW" Schmidt <frioux+cpan@gmail.com>
2012-11-24 21:40:03 +0000

    Import of FREW/App-Adenosine-2.000000 from CPAN.
    gitpan-cpan-distribution: App-Adenosine
    gitpan-cpan-version:      2.000000
    gitpan-cpan-path:         FREW/App-Adenosine-2.000000.tar.gz
    gitpan-cpan-author:       FREW
    gitpan-cpan-maturity:     released

---
+++ lib/App/Adenosine/Role/FiltersStdErr.pm
@@ -0,0 +1,35 @@
+package App::Adenosine::Role::FiltersStdErr;
+{
+  $App::Adenosine::Role::FiltersStdErr::VERSION = '2.000000';
+}
+
+use Moo::Role;
+
+requires 'filter_stderr';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+App::Adenosine::Role::FiltersStdErr
+
+=head1 VERSION
+
+version 2.000000
+
+=head1 AUTHOR
+
+Arthur Axel "fREW" Schmidt <frioux+cpan@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Arthur Axel "fREW" Schmidt.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ lib/App/Adenosine/Role/WrapsCurlCommand.pm
@@ -0,0 +1,35 @@
+package App::Adenosine::Role::WrapsCurlCommand;
+{
+  $App::Adenosine::Role::WrapsCurlCommand::VERSION = '2.000000';
+}
+
+use Moo::Role;
+
+requires 'wrap';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+App::Adenosine::Role::WrapsCurlCommand
+
+=head1 VERSION
+
+version 2.000000
+
+=head1 AUTHOR
+
+Arthur Axel "fREW" Schmidt <frioux+cpan@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Arthur Axel "fREW" Schmidt.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
